ab1e18ec82574666b442f3bb67d4b770a148229b
Floris Remmen <50661872+fl0r1s@users.noreply.github.com>
2019-09-11 20:57:58 +0200

    now only uploads printer.cfg :calibration settings

--- upload_calibration_settings.sh
+++ upload_calibration_settings.sh
@@ -4,12 +4,10 @@ echo "Uploading from pi to github..."
 
 echo "Moving files"
 cp -v  ~/printer.cfg ~/cr10_klipper/printer.cfg
-cp -v ~/print_macros.cfg ~/cr10_klipper/print_macros.cfg
 
 echo "Adding to git"
 git add printer.cfg
-git add print_macros.cfg
-git commit -m "uploaded from pi" 
+git commit -m "Uploaded calibration settings" 
 
 echo "Pushing..." 
 git push origin master
