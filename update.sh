02ebb619089c4826f442edfedbe7282c5f575797
Floris Remmen <50661872+fl0r1s@users.noreply.github.com>
2019-11-26 21:58:25 +0100

    changed copy method

--- update.sh
+++ update.sh
@@ -4,7 +4,11 @@
 # pull latest
 echo "Pulling..." 
 git pull https://github.com/fl0r1s/cr10_klipper/
+# Copy generic files
+echo "Copying generic files..." 
+cd generic
+cp -v generic/*.cfg ..
+
 # copy all files that end with cfg to 1 up
 echo "Copying config files for $PRINTER_NAME..." 
-cd $PRINTER_NAME
-cp -v *.cfg ../..
+cp -v $PRINTER_NAME/*.cfg ..
