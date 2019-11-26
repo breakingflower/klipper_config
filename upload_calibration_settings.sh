4aa77842b28577df55d2c4f3e45beec4ef22a552
Floris Remmen <50661872+fl0r1s@users.noreply.github.com>
2019-11-26 22:09:10 +0100

    added environment variable PRINTER_NAME

--- upload_calibration_settings.sh
+++ upload_calibration_settings.sh
@@ -3,10 +3,10 @@
 echo "Uploading from pi to github..." 
 
 echo "Moving files"
-cp -v  ~/printer.cfg ~/cr10_klipper/printer.cfg
+cp -v  ~/printer.cfg ~/cr10_klipper/$PRINTER_NAME/printer.cfg
 
 echo "Adding to git"
-git add printer.cfg
+git add $PRINTER_NAME/printer.cfg
 git commit -m "Uploaded calibration settings" 
 
 echo "Pushing..." 
