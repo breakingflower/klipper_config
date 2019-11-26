f2c96df7a6ca02ec5971af7b6ab91ecc3f29c990
Floris Remmen <50661872+fl0r1s@users.noreply.github.com>
2019-11-26 22:07:50 +0100

    removed wrong change directory. Uses PRINTER_NAME

--- update.sh
+++ update.sh
@@ -6,9 +6,8 @@ echo "Pulling..."
 git pull https://github.com/fl0r1s/cr10_klipper/
 # Copy generic files
 echo "Copying generic files..." 
-cd generic
-cp -v -r generic/ ..
+cp -v -r generic/*.cfg ~/ 
 
 # copy all files that end with cfg to 1 up
 echo "Copying config files for $PRINTER_NAME..." 
-cp -v -r $PRINTER_NAME ..
+cp -v -r $PRINTER_NAME/*.cfg ~/
