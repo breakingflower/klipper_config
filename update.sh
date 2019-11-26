c0d845a2c2fa9e2cd78a501a809fed0a829699fb
Floris Remmen <50661872+fl0r1s@users.noreply.github.com>
2019-11-26 22:00:20 +0100

    fixed copy method

--- update.sh
+++ update.sh
@@ -7,8 +7,8 @@ git pull https://github.com/fl0r1s/cr10_klipper/
 # Copy generic files
 echo "Copying generic files..." 
 cd generic
-cp -v generic/*.cfg ..
+cp -v generic/* ..
 
 # copy all files that end with cfg to 1 up
 echo "Copying config files for $PRINTER_NAME..." 
-cp -v $PRINTER_NAME/*.cfg ..
+cp -v $PRINTER_NAME/* ..
