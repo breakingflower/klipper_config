609fc95169be711dab14a90af62f7549c0458c5a
Floris Remmen <50661872+fl0r1s@users.noreply.github.com>
2019-12-26 16:27:55 +0100

    added a user choice to keep calib params in update

--- update.sh
+++ update.sh
@@ -8,6 +8,15 @@ git pull https://github.com/fl0r1s/cr10_klipper/
 echo "Copying generic files..." 
 cp -v -r generic/*.cfg ~/ 
 
+# ask if user wants to keep printer.cfg
+while true; do
+    read -p "Do you want to keep calibration parameters in printer.cfg?" yn
+    case $yn in
+        [Yy]* ) rm $PRINTER_NAME/printer.cfg; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 # copy all files that end with cfg to 1 up
 echo "Copying config files for $PRINTER_NAME..." 
 cp -v -r $PRINTER_NAME/*.cfg ~/
