8c4218d78da711944e70d65b7e048df5eaaea3f8
Floris Remmen <floris.remmen@gmail.com>
2020-11-04 08:50:09 +0000

    just copy the printer.cfg to root

--- update.sh
+++ update.sh
@@ -4,21 +4,11 @@
 echo "Backing up printer.cfg to printer.backup..." 
 mv ~/printer.cfg ~/printer.backup
 
-read -p "Do you want to clean home directory? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    rm ~/*.cfg
-fi
-
 # pull latest
 echo "Pulling..." 
 git pull https://github.com/fl0r1s/cr10_klipper/
-# Copy generic files
-#echo "Copying generic files..." 
-#cp -v -r generic/*.cfg ~/ 
 
-# copy all files that end with cfg to 1 up
+# copy printer.cfg for this printer
 echo "Copying the printer.cfg file for $PRINTER_NAME..." 
 cp -v -r ~/cr10_klipper/$PRINTER_NAME/printer.cfg ~/printer.cfg
 
